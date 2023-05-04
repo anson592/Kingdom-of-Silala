5d6e485124b92f97d73d2733949ae31f278f3108
bugyalu <774933704@qq.com>
2023-05-04 23:49:29 +0800

    feat: pb

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 
+import { useState } from "react";
 import {
   BrowserRouter as Router,
   Routes,
@@ -11,12 +12,17 @@ import "antd/dist/reset.css";
 import { useEffect } from "react";
 import beforeSrc from "@/assets/before.aac";
 import afterSrc from "@/assets/after.aac";
+import pbSrc from "@/assets/images/pb.png";
 
 const BGM = ({ children }: { children: React.ReactNode }) => {
   const match = !!useMatch("/");
+  const [show, setShow] = useState(true);
 
   useEffect(() => {
-    const bgm = new Audio(match ? beforeSrc : afterSrc);
+    if (match) {
+      return;
+    }
+    const bgm = new Audio(afterSrc);
     bgm.loop = true;
     bgm.play();
     return () => {
@@ -24,7 +30,36 @@ const BGM = ({ children }: { children: React.ReactNode }) => {
     };
   }, [match]);
 
-  return <div>{children}</div>;
+  useEffect(() => {
+    if (!match) {
+      return;
+    }
+
+    const bgm = new Audio(beforeSrc);
+    bgm.loop = true;
+    const hidden = () => {
+      setShow(false);
+      bgm.play();
+    };
+
+    document.addEventListener("mouseover", hidden);
+    return () => {
+      bgm.pause();
+      document.removeEventListener("mouseover", hidden);
+    };
+  }, [show, match]);
+
+  return (
+    <div>
+      {match && show && (
+        <img
+          src={pbSrc}
+          className={`fixed top-0 left-0 w-[100vw] h-[100vh] object-cover z-50 cursor-pointer`}
+        />
+      )}
+      {children}
+    </div>
+  );
 };
 
 function App() {
